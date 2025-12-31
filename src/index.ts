b382627f3dba2fe575d1853c2cf842d410c9cbb7
akachan <i@neri.moe>
2025-12-31 20:35:50 +0800

    何意为

--- src/index.ts
+++ src/index.ts
@@ -180,7 +180,8 @@ async function handleRegisterCmd(context: ActionContext, user?: string) {
 async function handleLoginCmd(context: ActionContext, user?: string) {
   const { error, userId } = await getTargetUserId(context, user);
   if (error) return error;
-  let [_, lockMessage] = await Promise.all([service.login(context, userId), handleLockCmd(context)]);
+  await service.login(context, userId);
+  let lockMessage = await handleLockCmd(context);
   let message = "✅ 入场成功\n\n" + lockMessage;
   return message;
 }
